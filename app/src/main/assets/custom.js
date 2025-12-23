e5889606aeca371685e7c3fd8f6b42870007abf2
zzxxqqdd <jordan.c@atlaschaos.com>
2025-12-23 15:31:07 +0800

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -1,10 +1,5 @@
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
-
-// very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
@@ -21,6 +16,11 @@ const hookClick = (e) => {
     } else {
         console.log('not handle origin', origin)
     }
+}
+
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
 }
 
 document.addEventListener('click', hookClick, { capture: true })
